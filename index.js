975081c6954cf72935436b64f04212a6efa77172
carolinafrederico <iam@carolinafrederico.com>
2025-03-14 22:12:36 -0400

    Part 5

--- index.js
+++ index.js
@@ -10,10 +10,27 @@ topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
 const menuLinks = [
-  { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  { text: 'about', href: '/about' },
+  {
+    text: 'catalog', href: '#', subLinks: [
+      { text: 'all', href: '/catalog/all' },
+      { text: 'top selling', href: '/catalog/top' },
+      { text: 'search', href: '/catalog/search' },
+    ]
+  },
+  {
+    text: 'orders', href: '#', subLinks: [
+      { text: 'new', href: '/orders/new' },
+      { text: 'pending', href: '/orders/pending' },
+      { text: 'history', href: '/orders/history' },
+    ]
+  },
+  {
+    text: 'account', href: '#', subLinks: [
+      { text: 'profile', href: '/account/profile' },
+      { text: 'sign out', href: '/account/signout' },
+    ]
+  },
 ];
 menuLinks.forEach((link) => {
   const anchor = document.createElement("a");
@@ -22,6 +39,51 @@ menuLinks.forEach((link) => {
   topMenuEl.appendChild(anchor);
 });
 //PART 2
-const subMenuEl = document.getElementById ("sub-menu");
+const subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+
+const topMenuLinks = document.querySelectorAll("a");
+
+// topMenuLinks.forEach((link) => {
+//   const anchor = document.createElement("a");
+//   anchor.setAttribute("href", link.href);
+//   anchor.textContent = link.text;
+//   subMenuEl.appendChild(anchor);
+//   if (link.subLinks) {
+//     link.subLinks.forEach((link) => {
+//       anchor.setAttribute("href", link.href);
+//       anchor.textContent = link.text;
+//       subMenuEl.appendChild(anchor);
+  //   }) 
+  // }
+  // console.log(topMenuLinks)
+// })
+topMenuEl.addEventListener("click", (event)=> {
+  event.preventDefault()
+  console.log(event.target.tagName)
+  if (event.target.tagName !== "A"){
+    return
+  } 
+  console.log(event.target.textContent)
+  event.target.classList.toggle("active")})
+  
+  //loop through the top menu link (variable)
+  topMenuLinks.forEach((link) => {
+    const anchor = document.createElement("a");
+      anchor.setAttribute("href", link.text);
+      anchor.textContent = link.text;
+      subMenuEl.appendChild(anchor);
+      if (link.subLinks) {
+            link.subLinks.forEach((link) => {
+              anchor.setAttribute("href", link.href);
+              anchor.textContent = link.text;
+              subMenuEl.appendChild(anchor);
+    });}
+
+  })
+  
